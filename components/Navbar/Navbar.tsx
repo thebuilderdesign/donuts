3524bd0c9158e9329b00afc0f312290c28b8a23e
thebuilderdesign <92240812+thebuilderdesign@users.noreply.github.com>
2023-03-05 20:29:53 +0700

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -25,7 +25,7 @@ export function Navbar() {
 
           <div className={styles.navMiddle}>
             <Link href="/buy" className={styles.link}>
-              Buy
+             Collect
             </Link>
             <Link href="/sell" className={styles.link}>
               Sell
