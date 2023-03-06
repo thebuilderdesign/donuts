dadd3dff1dafc5fe7843e9e63b45b99de8eddada
thebuilderdesign <92240812+thebuilderdesign@users.noreply.github.com>
2023-03-06 22:12:26 +0700

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,17 @@
-/* eslint-disable @next/next/no-img-element */
-import Link from "next/link";
 import React from "react";
 import { ImTelegram, ImTwitter }  from 'react-icons/im';
 import {GrMedium} from 'react-icons/gr'
 import styles from "./Footer.module.css";
+import Link from "next/link";
 
 function Footer() {
 	return (
 		<footer 
          className={styles.left}>
                <div className={styles.social}>
-               <a><ImTelegram href="https://telegram.org/" target="_blank" size={25}/></a>&nbsp; &nbsp;
-               <a><ImTwitter href="https://twitter.com/DonutsProtocol" target="_blank" size={25}/></a>&nbsp; &nbsp;
-               <a><GrMedium href="https://medium.com/" target="_blank"size={25}/></a>
+               <Link href="https://twitter.com/DonutsProtocol" target="_blank"><ImTwitter size={25}></ImTwitter></Link>&nbsp; &nbsp;
+               <Link href="https://telegram.org/" target="_blank"><ImTelegram size={25}></ImTelegram></Link>&nbsp; &nbsp;
+               <Link href="https://medium.com/" target="_blank"><GrMedium size={25}></GrMedium></Link>
                </div>
                <br></br>
                <br></br>
